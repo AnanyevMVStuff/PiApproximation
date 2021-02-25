2aab355a771359bef899334602629ee5f4acfb55
hellishsatan <36279374+hellishsatan@users.noreply.github.com>
2021-02-25 19:59:25 +0300

    Add files via upload

---
+++ PiApproximationApp.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+
+public class PiApproximationApp {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(()->{
+            PiApproximationFrame frame = new PiApproximationFrame();
+        });
+    }
+}
